061e68b22880549b8d7e16acf65989e1795f0f34
bayashi <bayashi@cpan.org>
2012-03-22 00:19:46 +0900

    initial commit

---
+++ lib/bayatest.pm
@@ -0,0 +1,56 @@
+package bayatest;
+use strict;
+use warnings;
+use Carp qw/croak/;
+
+our $VERSION = '0.01';
+
+sub new {
+    my $class = shift;
+    my $args  = shift || +{};
+
+    bless $args, $class;
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+bayatest - one line description
+
+
+=head1 SYNOPSIS
+
+    use bayatest;
+
+
+=head1 DESCRIPTION
+
+bayatest is
+
+
+=head1 REPOSITORY
+
+bayatest is hosted on github
+<http://github.com/bayashi/bayatest>
+
+
+=head1 AUTHOR
+
+Dai Okabayashi E<lt>bayashi@cpan.orgE<gt>
+
+
+=head1 SEE ALSO
+
+L<Other::Module>
+
+
+=head1 LICENSE
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
---
+++ t/01_basic.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+use Test::More 0.88;
+
+use bayatest;
+
+can_ok 'bayatest', qw/new/;
+
+# write more tests
+
+done_testing;
